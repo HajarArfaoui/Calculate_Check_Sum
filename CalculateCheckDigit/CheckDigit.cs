46e5e3245b9940474259807b16e892a872cfd97a
Hajar <hajararfaoui97@gmail.com>
2022-03-30 17:39:23 +0200

    adding files

---
+++ CalculateCheckDigit/CheckDigit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CalculateCheckDigit
+{
+    public class CheckDigit
+    {
+        public static string calculate_check_digit(string upcCode)
+        {
+            int sum = 0, checkDigit;
+            if (upcCode.Length != 11 || upcCode != (new Regex("[^0-9]")).Replace(upcCode, ""))
+            {
+                return "";
+            }
+            char[] characters = upcCode.ToCharArray();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                sum += i % 2 == 0 ? int.Parse(characters[i].ToString()) * 3 : int.Parse(characters[i].ToString());
+            }
+            checkDigit = sum % 10 == 0 ? 0 : 10 - sum % 10;
+            return String.Concat(upcCode, checkDigit);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CheckDigitTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculateCheckDigit;
+namespace CheckDigitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string str = CheckDigit.calculate_check_digit("71234567890");
+            Assert.AreEqual(str, "712345678904");
+        }
+        //validate check sum
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string str = CheckDigit.calculate_check_digit("01230000064");
+            Assert.AreEqual(str, "012300000642");
+        }
+        //validate UPC-A code format
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string str = CheckDigit.calculate_check_digit("7123@^@@4567890");
+            Assert.AreEqual(str, "");
+        }
+        //validate UPC-A code length
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string str = CheckDigit.calculate_check_digit("158896589963");
+            Assert.AreEqual(str, "");
+        }
+        // check Empty String
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string str = CheckDigit.calculate_check_digit("");
+            Assert.AreEqual(str, "");
+        }
+    }
+}
